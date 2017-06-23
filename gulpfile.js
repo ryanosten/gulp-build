a739546a67d0f35a42f79d5aa27e082e80359931
Ryan Osten <ostenrj@gmail.com>
2017-06-22 20:07:16 -0400

    added tasks

--- gulpfile.js
+++ gulpfile.js
@@ -6,6 +6,7 @@ var gulp = require('gulp'),
     rename = require('gulp-rename'),
     sass = require('gulp-sass'),
     maps = require('gulp-sourcemaps'),
+    imagemin = require('gulp-imagemin'),
     del = require('del');
 
 gulp.task('concatScripts', function(){
@@ -16,20 +17,42 @@ gulp.task('concatScripts', function(){
         .pipe(gulp.dest('js'));
 });
 
-gulp.task('minifyScripts', ['concatScripts'], function(){
+gulp.task('scripts', ['concatScripts'], function(){
   return gulp.src('js/app.js')
+        .pipe(maps.init())
         .pipe(uglify())
         .pipe(rename('all.min.js'))
-        .pipe(gulp.dest('js'));
+        .pipe(maps.write('./'))
+        .pipe(gulp.dest('dist/scripts'));
 });
 
+gulp.task('styles', function(){
+  return gulp.src('sass/global.scss')
+    .pipe(maps.init())
+    .pipe(sass())
+    .pipe(rename('all.min.css'))
+    .pipe(maps.write('./'))
+    .pipe(gulp.dest('dist/styles'))
+})
+
+gulp.task ('images', function(){
+  return gulp.src('images/*.{jpg,png}')
+    .pipe(imagemin())
+    .pipe(gulp.dest('dist/content'));
+})
+
+/*
 gulp.task('build', ['minifyScripts'], function(){
-  return gulp.src('js/app.min.js')
-    .pipe(gulp.dest('./dist'));
+  return gulp.src('js/all.min.js')
+    .pipe(gulp.dest('dist/scripts'));
 });
+*/
 
 gulp.task('default', function(){
   gulp.start('build');
-})
+});
+
+//.pipe(maps.init())
+//.pipe(maps.write('./'))
 
 //next task, create dist folder
